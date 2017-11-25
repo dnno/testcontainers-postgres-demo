152e2553e13425d243550ad34a48ea3a51e138b1
Reinhard Prechtl <reinhard.prechtl@codecentric.de>
2017-11-25 13:14:14 +0100

    Initial

---
+++ src/main/java/de/rpr/testcontainerspostgresdemo/TestcontainersPostgresDemoApplication.java
@@ -0,0 +1,12 @@
+package de.rpr.testcontainerspostgresdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersPostgresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersPostgresDemoApplication.class, args);
+	}
+}
